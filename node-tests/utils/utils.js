ab4fffbe4360fa27d38c0c45c71e21c390ec9b93
Nitin SINGH <nitinkumar.singh@f5.com>
2018-11-12 18:20:10 +0530

    test the method and async fuctions and initial setup for express app

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,3 +1,17 @@
 module.exports.add = (a, b) => a + b;
 
+module.exports.asyncAdd = (a, b, cb) => {
+    setTimeout(() => {
+        cb(a+b);
+    }, 1000);
+}
+
 module.exports.square = x => (x * x).toString();
+
+module.exports.setName = (user, fullName) => {
+    const names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+
+    return user;
+}
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -11,4 +11,53 @@ it('should square the input', ()=>{
     // if(utils.square(6) !== 36) throw new Error(`Not doing the square ???`)
 
     expect(utils.square(6)).toBe('36').toBeA('string')
+});
+
+
+it('should expect some values', ()=>{
+    // expect(12).toNotBe(11);
+
+    // to be does work well for objects and arrays
+    // expect({name: 'Nitin'}).toBe({name:'Nitin'})
+
+    expect({name:'Kapil'}).toEqual({name:'Kapil'})
+    expect({name:'Kapil'}).toNotEqual({name:'Kapil1'})
+
+    expect([1,2,3]).toInclude(2);
+    expect([1,2,4,6]).toExclude(5);
+
+    expect({
+        name: 'Nitin',
+        age: 29,
+        location:'Hyderabad',
+    }).toInclude({
+        age:29
+    });
+
+    expect({
+        name: 'Nitin',
+        age: 29,
+        location:'Hyderabad',
+    }).toExclude({
+        age:22
+    });
+});
+
+
+// should verify first and last names are set
+it('should set first name and last name', ()=>{
+    const user = {age: 29, location: 'Hyderabad' };
+    const returnUser = utils.setName(user, 'Nitin');
+    expect(returnUser).toEqual(user);
+    expect(returnUser).toInclude({firstName: 'Nitin', lastName: undefined});
+
 })
+
+// Testing async code;
+it('should async add two numbers',(done)=>{
+    utils.asyncAdd(4, 3, (sum)=>{
+        expect(sum).toBe(7).toBeA('number')
+        done(); // if you dont do this this will fail with timeout sec exceeded. 
+    });
+    
+});
