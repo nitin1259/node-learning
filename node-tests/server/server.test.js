7293e24aba7c186f22dea939d54c92ab3a94d1b5
Nitin SINGH <nitinkumar.singh@f5.com>
2018-11-12 20:36:31 +0530

    introducing describe method from mocha...

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const expect = require('expect');
 
 const app = require('./server').app;
-
+/*
 it('should return hello world response', (done) => {
     request(app).get('/').expect(200).expect('Hello world !').end(done);
 })
@@ -39,3 +39,54 @@ it('should return my user object', (done) => {
         expect(res.body).toInclude({ name: 'Nitin', age: 30 });
     }).end(done);
 })
+
+*/
+
+
+
+
+describe('Server Test', ()=>{
+
+    describe('#basic url test', ()=>{
+        it('should return hello world response', (done) => {
+            request(app).get('/').expect(200).expect('Hello world !').end(done);
+        });
+    })
+    
+    
+    // it('should return hello world response error return one', (done)=>{
+    //     request(app).get('/').expect(200).expect('Hello world !!!').end(done);
+    // })
+    
+    // it('should return hello world response error status one', (done)=>{
+    //     request(app).get('/').expect(404).expect('Hello world !').end(done);
+    // })
+    
+    describe('#Test testing', ()=>{
+        it('should return Testing one with erro code 404', (done) => {
+            request(app).get('/test1').expect(404).expect('Testing one !').end(done);
+        })
+        
+        it('should return Testing one with error object', (done) => {
+            request(app).get('/test2').expect(404).expect({
+                error: 'page not found'
+            }).end(done);
+        });
+        
+        
+        it('should return Testing one ! check assertion with expect library ', (done) => {
+            request(app).get('/test3').expect(404).expect((res) => {
+                expect(res.body).toInclude({ name: 'Todo app v1.0' })
+            }).end(done);
+        });
+    });
+    
+    describe('#user test', ()=>{
+        it('should return my user object', (done) => {
+            request(app).get('/users').expect(200).expect((res) => {
+                expect(res.body).toInclude({ name: 'Nitin', age: 30 });
+            }).end(done);
+        })
+    });
+    
+});
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,6 +1,7 @@
 const utils = require('./utils');
 const expect = require('expect');
 
+/*
 it('should add two number', () => {
     const res = utils.add(55, 22);
     expect(res).toBe(77).toBeA('number')
@@ -60,4 +61,90 @@ it('should async add two numbers',(done)=>{
         done(); // if you dont do this this will fail with timeout sec exceeded. 
     });
     
+});
+*/
+
+
+/**
+ * 
+ * introuducing describe - described is a function injected into your test files just like it(). It comes from a moka and it's really fantastic.
+ * Essentially it lets you group tasks together that makes it a lot easier to scan the test output.
+ * 
+ */
+
+
+
+describe('Utils tests', () => {
+
+    describe('#add', () => {
+
+        it('should add two number', () => {
+            const res = utils.add(55, 22);
+            expect(res).toBe(77).toBeA('number')
+            // if(res !== 77) throw new Error(`expected 77, but getting ${res}`);
+        });
+
+        // Testing async code;
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number')
+                done(); // if you dont do this this will fail with timeout sec exceeded. 
+            });
+
+        });
+
+    });
+
+    describe('#square', () => {
+
+        it('should square the input', () => {
+            // if(utils.square(6) !== 36) throw new Error(`Not doing the square ???`)
+
+            expect(utils.square(6)).toBe('36').toBeA('string')
+        });
+
+    });
+
+    describe('Random test for expect', () => {
+        it('should expect some values', () => {
+            // expect(12).toNotBe(11);
+
+            // to be does work well for objects and arrays
+            // expect({name: 'Nitin'}).toBe({name:'Nitin'})
+
+            expect({ name: 'Kapil' }).toEqual({ name: 'Kapil' })
+            expect({ name: 'Kapil' }).toNotEqual({ name: 'Kapil1' })
+
+            expect([1, 2, 3]).toInclude(2);
+            expect([1, 2, 4, 6]).toExclude(5);
+
+            expect({
+                name: 'Nitin',
+                age: 29,
+                location: 'Hyderabad',
+            }).toInclude({
+                age: 29
+            });
+
+            expect({
+                name: 'Nitin',
+                age: 29,
+                location: 'Hyderabad',
+            }).toExclude({
+                age: 22
+            });
+        });
+    });
+
+    describe('#Object Test', () => {
+        // should verify first and last names are set
+        it('should set first name and last name', () => {
+            const user = { age: 29, location: 'Hyderabad' };
+            const returnUser = utils.setName(user, 'Nitin');
+            expect(returnUser).toEqual(user);
+            expect(returnUser).toInclude({ firstName: 'Nitin', lastName: undefined });
+
+        });
+    });
+
 });
