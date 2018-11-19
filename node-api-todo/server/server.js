fd26c8918e62f6ed4d399a7991d3592be8e1b1f3
Nitin SINGH <nitinkumar.singh@f5.com>
2018-11-19 20:57:41 +0530

    validators, type and defaults

--- node-api-todo/server/server.js
+++ node-api-todo/server/server.js
@@ -7,32 +7,77 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/ToDoApp');
 
 const ToDos = mongoose.model('ToDos', {
-    text: { type: String },
-    completed: { type: Boolean },
-    completeAt: { type: Number }
+    text: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completeAt: {
+        type: Number,
+        default: null
+    }
 });
 
 
-const newTodo = new ToDos({
-    text: 'Cook dinner'
+// const newTodo = new ToDos({
+//     text: 'Cook dinner'
+// });
+
+
+// newTodo.save().then(res => {
+//     console.log('create a todo collection document ', res);
+// }, err => {
+//     console.log('error while saving todos: ', err)
+// });
+
+
+// const anotherTodo = new ToDos({
+//     text: 'go to gym',
+//     completed: false,
+//     completeAt: 0
+// });
+
+
+// anotherTodo.save().then(res => {
+//     console.log('create a todo collection document ', res);
+// }).catch(err => {
+//     console.log('error while saving todos: ', err);
+// })
+
+/*
+const todo1 = new ToDos({
+    text: 'Something has to do'
 });
 
-
-newTodo.save().then(res => {
+todo1.save().then(res => {
     console.log('create a todo collection document ', res);
-}, err => {
-    console.log('error while saving todos: ', err)
-});
+}).catch(err => {
+    console.log('error while saving todos: ', err);
+})
+
+*/
+
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
+    }
+})
 
 
-const anotherTodo = new ToDos({
-    text: 'go to gym',
-    completed: false,
-    completeAt: 0
-});
+const user1 = new User({
+    email  : 'jaat.nitin19@gmail.com'
+})
 
 
-anotherTodo.save().then(res => {
+user1.save().then(res => {
     console.log('create a todo collection document ', res);
 }).catch(err => {
     console.log('error while saving todos: ', err);
